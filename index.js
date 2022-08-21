c1b04bd6d7c94ea4e8384d285859ccc8c2b3793e
Tanya Tarinskaya <36923363+tantarin@users.noreply.github.com>
2022-08-21 18:23:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+express = require('express');
+var moment = require('moment');
+
+app = express();
+const format = "YYYY-MM-DD";
+
+var date = moment(new Date()).format(format);
 require('http').Server((req, res) => {
     if (req.url == '/163077') {
+      
       res.writeHead(200, { 'Content-Type': 'application/json', 'exam': 'Web' });
-      return res.end(JSON.stringify({"date": "2022-08-21"}));
+      return res.end(JSON.stringify({"date": date}));
     }
     res.end('NULL');
   })
