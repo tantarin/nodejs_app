7877afc1eae7ca397954aca32059f256de98a6a7
Tanya Tarinskaya <36923363+tantarin@users.noreply.github.com>
2022-08-21 18:34:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const format = "YYYY-MM-DD";
 var date = moment(new Date()).format(format);
 require('http').Server((req, res) => {
     if (req.url == '/163077') {
-      
       res.writeHead(200, { 'Content-Type': 'application/json', 'exam': 'Web' });
       return res.end(JSON.stringify({"date": date}));
     }
-    res.end('NULL');
+    res.writeHead(404, {"Content-Type":'application/json', 'exam': 'Web'});
+    res.end();
   })
   .listen(process.env.PORT || 5000);
